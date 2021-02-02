50e291393cc67dd2332d59a0d9ecf8a33ca6534a
Taigah <taigah.dev@gmail.com>
2021-02-02 02:27:53 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  separator: '_',
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans: ['"Alata"', 'sans'],
+      title: ['"Josefin Sans"', 'sans']
+    },
+    extend: {
+      fontSize: {
+        body: '0.9375rem'
+      },
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        black: 'hsl(0, 0%, 0%)',
+        'dark-gray': 'hsl(0, 0%, 55%)',
+        'very-dark-gray': 'hsl(0, 0%, 41%)'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
